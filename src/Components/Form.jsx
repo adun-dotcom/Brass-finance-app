e1a5632beeb05b53c1f01f75a6563e65140c0b6c
Adunola Johnson Odetola <aduntobi7@gmail.com>
2021-04-19 08:01:17 +0100

    modify file

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,103 +1,103 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from './Button/Button'
-const emailRegex = RegExp(
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{&}~-]+@[a-zA-Z0-9-]+.(?:\.[a-zA-Z0-9-]+)*$/
-)
-export default class Form extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: null,
-      password: null,
-      disabled: false,
-      press: false,
-      formError: {
-        email: '',
-        password: '',
-      },
-    }
-  }
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import { Button } from './Button/Button'
+// const emailRegex = RegExp(
+//   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{&}~-]+@[a-zA-Z0-9-]+.(?:\.[a-zA-Z0-9-]+)*$/
+// )
+// export default class Form extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       email: null,
+//       password: null,
+//       disabled: false,
+//       press: false,
+//       formError: {
+//         email: '',
+//         password: '',
+//       },
+//     }
+//   }
 
-  handleChange = (e) => {
-    e.preventDefault()
-    const { name, value } = e.target
-    const formError = this.state.formError
-    switch (name) {
-      case 'email':
-        formError.email = emailRegex.test(value)
-          ? '' : 'Please enter a valid email address'
-        break
-      case 'password':
-        formError.password = value.length < 6 ? 'minimum password is 6' : ''
-      default:
-        break
-    }
-    this.setState({ formError, [name]: value })
-  }
+//   handleChange = (e) => {
+//     e.preventDefault()
+//     const { name, value } = e.target
+//     const formError = this.state.formError
+//     switch (name) {
+//       case 'email':
+//         formError.email = emailRegex.test(value)
+//           ? '' : 'Please enter a valid email address'
+//         break
+//       case 'password':
+//         formError.password = value.length < 6 ? 'minimum password is 6' : ''
+//       default:
+//         break
+//     }
+//     this.setState({ formError, [name]: value })
+//   }
   
-  handleClick = (e) => {
-    e.preventDefault()
-    this.setState({disabled: true})
-  }
-  render() {
-    const formError = { ...this.state.formError }
-    return (
-      <div className="wrapper">
-        <div className="form-wrapper">
-          <h1>Log in to your account</h1>
-          <form onSubmit={this.handleClick}>
-            <div className="error-field">
-              <small>{formError.email}</small>
-              <br />
-              <input
-                type="email"
-                name="email"
-                placeholder="Email Address"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="error-field">
-              <small>{formError.password}</small>
-              <br />
-              <input
-                type="password"
-                name="password"
-                placeholder="Enter Password"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="btn-submit">
-              {this.state.disabled &&
-             this.state.email &&
-              this.state.password? 
-                <Link to="/Dashboard">
-                  <Button
-                    type="submit"
-                    btnText="Log me in"
-                    width="100%"
-                    padding="1.5rem"
+//   handleClick = (e) => {
+//     e.preventDefault()
+//     this.setState({disabled: true})
+//   }
+//   render() {
+//     const formError = { ...this.state.formError }
+//     return (
+//       <div className="wrapper">
+//         <div className="form-wrapper">
+//           <h1>Log in to your account</h1>
+//           <form onSubmit={this.handleClick}>
+//             <div className="error-field">
+//               <small>{formError.email}</small>
+//               <br />
+//               <input
+//                 type="email"
+//                 name="email"
+//                 placeholder="Email Address"
+//                 value={this.state.email}
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+//             <div className="error-field">
+//               <small>{formError.password}</small>
+//               <br />
+//               <input
+//                 type="password"
+//                 name="password"
+//                 placeholder="Enter Password"
+//                 value={this.state.password}
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+//             <div className="btn-submit">
+//               {this.state.disabled &&
+//              this.state.email &&
+//               this.state.password? 
+//                 <Link to="/Dashboard">
+//                   <Button
+//                     type="submit"
+//                     btnText="Log me in"
+//                     width="100%"
+//                     padding="1.5rem"
                     
-                  />
-                </Link>
-               : 
-                <Button
-                  type="submit"
-                  btnText="Log me in"
-                  width="100%"
-                  padding="1.5rem"
-                />
-              }
-            </div>
+//                   />
+//                 </Link>
+//                : 
+//                 <Button
+//                   type="submit"
+//                   btnText="Log me in"
+//                   width="100%"
+//                   padding="1.5rem"
+//                 />
+//               }
+//             </div>
 
-            <span>
-              Forgot your password? <a href="#">Reset it here</a>
-            </span>
-          </form>
-        </div>
-      </div>
-    )
-  }
-}
+//             <span>
+//               Forgot your password? <a href="#">Reset it here</a>
+//             </span>
+//           </form>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
